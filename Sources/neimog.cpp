18686496d9f53e8e613af5c3560af735aa486980
Charles K. Neimog <charlesneimog@outlook.com>
2024-04-11 12:03:56 -0300

    add some sounds

--- Sources/neimog.cpp
+++ Sources/neimog.cpp
@@ -1,5 +1,9 @@
 #include "neimog.hpp"
 
+extern "C" {
+#include <s_stuff.h>
+}
+
 static t_class *neimogLib;
 
 static void *neimogNew(void) {
@@ -18,12 +22,20 @@ extern "C" void setup_pd0x2dneimog(void) {
     neimogLib = class_new(gensym("pd-neimog"), (t_newmethod)neimogNew, 0, sizeof(Neimog),
                           CLASS_NOINLET, A_NULL, 0);
 
+    // add to the search path
+    sys_lock(); // following code from libpd
+    std::string libPath = neimogLib->c_externdir->s_name;
+    std::string AbsPath = libPath + "/Abstractions/";
+    std::string AudiosPath = libPath + "/Resources/Audios/";
+    STUFF->st_searchpath = namelist_append(STUFF->st_searchpath, libPath.c_str(), 0);
+    STUFF->st_searchpath = namelist_append(STUFF->st_searchpath, AbsPath.c_str(), 0);
+    STUFF->st_searchpath = namelist_append(STUFF->st_searchpath, AudiosPath.c_str(), 0);
+    sys_unlock();
+
     ArrayRotateSetup();
     ArraySumSetup();
     musesampler_tilde_setup();
     ambi_tilde_setup();
 
-    post("");
     post("[pd-neimog] version %d.%d.%d", 0, 0, 1);
-    post("");
 }
